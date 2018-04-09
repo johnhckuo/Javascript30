a431dfc2eef3014567ccc432614e7c331c5eba9e
johnhckuo <johnhckuo@gmail.com>
2018-04-10 00:29:03 +0800

    react-router-dom init

--- src/script/index.jsx
+++ src/script/index.jsx
@@ -1,14 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Test from "./components/test/main"
+import Drum from "./components/01 - JavaScript Drum Kit/main"
 
 import store from "./store"
-// import "../style/reset.scss"
+import "../style/normalize.css"
 
 const app = document.getElementById('app')
 
 ReactDOM.render(<Provider store={store}>
-	<Test />
+<Router>
+<div>
+	<ul>
+		<li><Link to="/">Index</Link></li>
+		<li><Link to="/01">01</Link></li>
+	</ul>
+	<hr/>
+	<Route exact path="/" component={Test}></Route>
+	<Route path="/01" component={Drum} ></Route>
+</div>
+</Router>
 </Provider>, app);
--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src/index.html')
     }),
-    new OpenBrowserPlugin({ url: 'http://localhost:8080' })
-  ]
+    new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
+    new ExtractTextPlugin( "bundle.css" )
+
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
 };
