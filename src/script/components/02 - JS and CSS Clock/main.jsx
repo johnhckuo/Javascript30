3a1f53c6253a317e5f73caf25c376a873f5c318e
johnhckuo <johnhckuo@gmail.com>
2018-04-16 00:25:24 +0800

    day2 init

--- src/script/components/02 - JS and CSS Clock/main.jsx
+++ src/script/components/02 - JS and CSS Clock/main.jsx
@@ -5,12 +5,26 @@ import * as Style from "./style"
 export default class Clock extends React.Component{
   constructor(){
     super();
+    this.getCurrentTime = this.getCurrentTime.bind(this);
+  }
+
+  componentDidMount(){
+    this.counter = setInterval(this.getCurrentTime, 1000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.counter);
+  }
+
+  getCurrentTime(){
+    let d = new Date();
+    let hour = d.getHours();
+    let minute = d.getMinutes();
+    let second = d.getSeconds();
+    console.log(hour, minute, second)
   }
 
   render(){
     return (<div></div>);
   }
-
-
-
 }
